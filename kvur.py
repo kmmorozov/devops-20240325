280cd43170ec8902630d0c8f62e89001e2f2c0ea
morozovkm <morozovkm@yandex.ru>
2024-03-25 11:03:30 +0300

    Заставил пользователя ввести корректные данные, обработал ошибки

--- kvur.py
+++ kvur.py
@@ -1,1 +1,16 @@
 print("Вас приветствует система решения квадратных уранений!")
+print("Для решения уравнения введите слудующие значения в виде числа!")
+bad_data = True
+while bad_data == True:
+    try:
+        a = int(input("Введите a: "))
+        b = int(input("Введите b: "))
+        c = int(input("Введите c: "))
+        bad_data = False
+    except ValueError:
+        print('Данные не верны!')
+    except ZeroDivisionError:
+        print('На ноль делить нельзя!')
+    except:
+        print('Какая-то ошибка!')
+
